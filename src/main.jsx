2167571ab2e003849e7c2a8f1bd1474eb61624b2
unknown <stevanoswh@gmail.com>
2024-03-18 10:34:44 +0700

    changes in routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import './index.scss'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx';
 import Contact from './pages/Contact.jsx';
 import NotFoundPage from './pages/NotFound.jsx';
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>} errorElement={<NotFoundPage/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='contact' element={<Contact/>}/>
-    </Route>
-
-  )
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
